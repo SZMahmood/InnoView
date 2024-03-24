0827d424f30e8304d9902205683d9208b73efa11
blue3212 <luckywill339@gmail.com>
2024-03-24 15:00:50 -0400

    Dockerization and run from index.js

--- InnoView/backend/index.js
+++ InnoView/backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require('path');
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -7,6 +8,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
+
+app.use(express.static(path.join(__dirname, '../dist/inno-view/browser')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/inno-view/browser/index.html'));
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
