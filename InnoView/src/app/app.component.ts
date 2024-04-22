7b386f640e38e41678584a7427c6093ac8108f81
SZMahmood <122752984+SZMahmood@users.noreply.github.com>
2024-04-22 17:43:54 -0500

    fixed display

--- InnoView/src/app/app.component.ts
+++ InnoView/src/app/app.component.ts
@@ -1,10 +1,12 @@
+import { ViewEncapsulation } from '@angular/core';
 import { Component } from '@angular/core';
 //import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'InnoView';
--- InnoView/src/app/home/home.component.ts
+++ InnoView/src/app/home/home.component.ts
@@ -1,16 +1,13 @@
+import { ViewEncapsulation } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder} from '@angular/forms';
-
-
-
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  encapsulation: ViewEncapsulation.None
 })
-
-
 
 export class HomeComponent{
   
